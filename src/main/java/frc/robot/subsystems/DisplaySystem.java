c907ac7c0c860fe4191532aef74b7369ce2e0800
Marnie Francisco <team3024programming@outlook.com>
2022-09-05 15:08:11 -0700

    changed the way some arrays print to dash

--- src/main/java/frc/robot/subsystems/DisplaySystem.java
+++ src/main/java/frc/robot/subsystems/DisplaySystem.java
@@ -19,29 +19,27 @@ public class DisplaySystem extends SubsystemBase {
   }
 
   public void printValueToDash(String label, double value) {
-    if (Constants.DUBUG_MODE) {
+    if (Constants.DUBUG_MODE)
       SmartDashboard.putNumber(label, value);
-    }
   }
 
   public void printArrayToDash(String label, double[] valueArray) {
-    if (Constants.DUBUG_MODE) {
-      SmartDashboard.putNumberArray(label, valueArray);
-    }
+    //if (Constants.DUBUG_MODE) {
+    //  SmartDashboard.putNumberArray(label, valueArray);
+    //}
+    for (int i=0; i<4; i++)
+      printValueToDash(Robot.driveTrain.driveModules[i].location+label, valueArray[i]);
   }
   
   public void printVectorToDash(String label, double[] vector) {
-    if (Constants.DUBUG_MODE) {
+    if (Constants.DUBUG_MODE)
       SmartDashboard.putNumberArray(label, vector);
-    }
   }
 
   public void printVectorsToDash(String label, double[][] vectorArray) {
-    if (Constants.DUBUG_MODE) {
-      for (int i=0; i<vectorArray.length; i++) {
+    if (Constants.DUBUG_MODE)
+      for (int i=0; i<vectorArray.length; i++)
         SmartDashboard.putNumberArray(Robot.driveTrain.driveModules[i].location+" "+label, vectorArray[i]);
-      }
-    }
   }
 
   public void printAnglesToDash() {
@@ -50,16 +48,16 @@ public class DisplaySystem extends SubsystemBase {
     for (int i=0; i<4; i++) {
       currentMotorAngles[i] = Robot.driveTrain.driveModules[i].currentAngle;
       angleMotorOutputs[i] = Robot.driveTrain.driveModules[i].angleMotorOutput;
+      String currentModule = Robot.driveTrain.driveModules[i].location;
+      printValueToDash(currentModule+" current angle", currentMotorAngles[i]);
+      printValueToDash(currentModule+" angle output", angleMotorOutputs[i]);  
     }
-    printArrayToDash("current angle", currentMotorAngles);
-    printArrayToDash("angle output", angleMotorOutputs);
   }
 
   public void printSpeedsToDash() {
     double[] speedMotorOutputs = new double[4];
-    for (int i=0; i<4; i++) {
+    for (int i=0; i<4; i++)
       speedMotorOutputs[i] = Robot.driveTrain.driveModules[i].speedMotorOutput;
-    }
     printArrayToDash("speed output", speedMotorOutputs);
   }
 
